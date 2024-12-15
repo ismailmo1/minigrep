246301f23705e68ebdd27ab07a03291709baf4df
Ismail Mohammed <ismailmo4@gmail.com>
2024-12-15 22:08:02 +0000

    fin chap 12

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,16 @@
-use::std::env;
-use::std::fs;
+use ::std::env;
+use minigrep::Config;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = parse_config(&args);
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        eprintln!("problem parsing arguments {err}");
+        process::exit(1);
+    });
 
-    println!("searching for {} in {}", config.query, config.file_path);
-    let file_contents = fs::read_to_string(&config.file_path).expect("should've been able to read file");
-    println!("with contents:\n{}", file_contents);
-}
-
-struct Config{
-    query: String,
-    file_path:String,
-}
-
-fn parse_config(args: &[String])->Config{
-    let query = args[1].clone();
-    let file_path = args[2].clone();
-    return Config { query, file_path }
+    if let Err(e) = minigrep::run(config) {
+        eprintln!("application error {e}");
+        process::exit(1);
+    };
 }
